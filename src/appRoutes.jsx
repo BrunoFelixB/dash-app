6758598c682b159af73f3c5238ffa7f70bfd4124
BrunoFelixB <brunelix5@gmail.com>
2022-09-08 22:38:02 -0300

    bug fix

--- src/appRoutes.jsx
+++ src/appRoutes.jsx
@@ -7,7 +7,7 @@ import {
     Navigate
 } from 'react-router-dom'
 
-import { Login } from "./pages/login/login"
+import { Login } from "./pages/login/Login"
 import { Loginreset } from './pages/Loginreset/Loginreset'
 
 import { Dashboard } from "./pages/dashboard/dashboard"
